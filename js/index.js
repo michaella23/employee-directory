f5e6721f8ef772925816877234a03c8cf3f6363c
Michaella Rodriguez <micha.ella@hotmail.com>
2023-03-12 23:57:07 -0500

    filter functionality

--- js/index.js
+++ js/index.js
@@ -1,22 +1,50 @@
 import employees from "/js/employees.js"
 
-const mainContent = employees.map((employee) => {
+const mainContent = employees.map(({image, name, title, bio, social}) => {
     return `
         <div class="employee-block">
-            <img src="images/photos/${employee.image}" alt="headshot of ${employee.name}" class="emp-img" />
-            <h2>${employee.name}</h2>
-            <h3>${employee.title}</h3>
-            <div class="bio">${employee.bio}</div>
+            <img src="images/photos/${image}" alt="headshot of ${name}" class="emp-img" />
+            <h2>${name}</h2>
+            <h3>${title}</h3>
+            <div class="bio">${bio}</div>
             <div class="socials">                
-                <a href="${employee.social.linkedin}" target="_blank" class="link"><img src="images/icons/linkedIn.png" alt="" class="social-icon" /></a>
-                <a href=${employee.social.twitter} target="_blank" class="link"><img src="images/icons/twitter.png" alt="" class="social-icon" /></a> 
+                <a href="${social.linkedin}" target="_blank" class="link"><img src="images/icons/linkedIn.png" alt="" class="social-icon" /></a>
+                <a href=${social.twitter} target="_blank" class="link"><img src="images/icons/twitter.png" alt="" class="social-icon" /></a> 
             </div>       
         </div>
     `
 }).join('');
 
-document.body.innerHTML += `
-    <main class="main">
-        ${mainContent}
-    </main>
-`
+let mainEl = document.createElement("section")
+mainEl.setAttribute("class", "main")
+mainEl.innerHTML = mainContent
+document.body.appendChild(mainEl)
+
+const filterMenu = document.getElementById("filter-menu");
+
+filterMenu.addEventListener("change", function (event) {
+        if (event.target.value === "everyone") {
+            mainEl.innerHTML = mainContent;
+        } else {
+        mainEl.innerHTML = ""        
+        employees.filter(employee => {
+            if (employee.team === event.target.value)
+                return employee
+        }).map(({image, name, title, bio, social}) => {
+                mainEl.innerHTML +=
+                    `
+                        <div class="employee-block">
+                            <img src="images/photos/${image}" alt="headshot of ${name}" class="emp-img" />
+                            <h2>${name}</h2>
+                            <h3>${title}</h3>
+                            <div class="bio">${bio}</div>
+                            <div class="socials">                
+                                <a href="${social.linkedin}" target="_blank" class="link"><img src="images/icons/linkedIn.png" alt="" class="social-icon" /></a>
+                                <a href=${social.twitter} target="_blank" class="link"><img src="images/icons/twitter.png" alt="" class="social-icon" /></a> 
+                            </div>       
+                        </div>
+                    `
+            })
+        return mainEl;
+        }
+})
